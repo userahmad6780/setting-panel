2eb3dd47511003dcf1d7acbc04f5282724f4ab70
userahmad6780 <mahmadctn38@gmail.com>
2025-04-18 01:54:16 +0500

    feat: added products to the products page

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -5,8 +5,20 @@ import arrow_icon from './arrow_icon.svg'
 import pk_flg_icon from './pakistan_flag.png'
 import german_flg_icon from './german_flag.png'
 import uk_flg_icon from './united-kingdom-flag.jpg'
-
-
+import nft1 from './nfts/nft1.webp'
+import nft2 from './nfts/nft2.png'
+import nft3 from './nfts/nft3.webp'
+import nft4 from './nfts/nft4.png'
+import nft5 from './nfts/nft5.png'
+import nft6 from './nfts/nft6.png'
+import nft7 from './nfts/nft7.png'
+import nft8 from './nfts/nft8.png'
+import nft9 from './nfts/nft9.png'
+import nft10 from './nfts/nft10.png'
+import nft11 from './nfts/nft11.webp'
+import nft12 from './nfts/nft12.png'
+import nft13 from './nfts/nft13.jpg'
+import nft14 from './nfts/nft14.webp'
 
 export const assets = {
     logo,
@@ -17,3 +29,91 @@ export const assets = {
     german_flg_icon,
     uk_flg_icon
 }
+
+export const nfts = [
+    {
+      _id: 'nft1',
+      name: 'Captain Chaos',
+      image: nft1,
+      price: '$120',
+    },
+    {
+      _id: 'nft2',
+      name: 'Pixel Bandit',
+      image: nft2,
+      price: '$85',
+    },
+    {
+      _id: 'nft3',
+      name: 'Choco Krusher',
+      image: nft3,
+      price: '$150',
+    },
+    {
+      _id: 'nft4',
+      name: 'Blinky the Menace',
+      image: nft4,
+      price: '$99',
+    },
+    {
+      _id: 'nft5',
+      name: 'Ziggy the Zap',
+      image: nft5,
+      price: '$180',
+    },
+    {
+      _id: 'nft6',
+      name: 'Neko Ninja',
+      image: nft6,
+      price: '$110',
+    },
+    {
+      _id: 'nft7',
+      name: 'Panda Punk',
+      image: nft7,
+      price: '$135',
+    },
+    {
+      _id: 'nft8',
+      name: 'Sir Quack of Doom',
+      image: nft8,
+      price: '$95',
+    },
+    {
+      _id: 'nft9',
+      name: 'Milo the Mischief',
+      image: nft9,
+      price: '$160',
+    },
+    {
+      _id: 'nft10',
+      name: 'Toasty Terror',
+      image: nft10,
+      price: '$125',
+    },
+    {
+      _id: 'nft11',
+      name: 'Wiggles the Wild',
+      image: nft11,
+      price: '$170',
+    },
+    {
+      _id: 'nft12',
+      name: 'Sproutzilla',
+      image: nft12,
+      price: '$145',
+    },
+    {
+      _id: 'nft13',
+      name: 'Giggles the Gremlin',
+      image: nft13,
+      price: '$75',
+    },
+    {
+      _id: 'nft14',
+      name: 'Bubble Trouble',
+      image: nft14,
+      price: '$200',
+    }
+];
+  
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
+import { nfts } from '../assets/assets'
+import { FiShoppingCart } from "react-icons/fi";
 
 const Products = () => {
   return (
     <div className="relative isolate px-6 pt-14 lg:px-8">
-      <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-        <div className="text-center">
-          <h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 dark:text-white sm:text-7xl">
-            Products Page
-          </h1>
+      <div className="mx-auto max-w-6xl pt-16 sm:pt-20 lg:pt-36">
+        <h2 className="sr-only">Products</h2>
+        <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 xl:gap-x-8">
+          {nfts.map((nft) => (
+            <div key={nft._id} className="group">
+              <img
+                alt={nft.name}
+                src={nft.image}
+                className="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-7/8"
+              />
+              <div className='flex justify-between items-baseline-last'>
+                <div>
+                  <h3 className="mt-4 text-sm text-gray-700 dark:text-gray-400">{nft.name}</h3>
+                  <p className="mt-1 text-lg font-medium text-gray-900 dark:text-white">{nft.price}</p>
+                </div>
+                <FiShoppingCart className='text-lg dark:text-white'/>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
